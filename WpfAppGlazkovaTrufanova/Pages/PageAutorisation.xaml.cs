8375d4a74373c6bf07e67f446c884fe2513d0dfc
admin <admin@44-10>
2024-11-11 15:29:42 +0300

    Добавьте файлы проекта.

---
+++ WpfAppGlazkovaTrufanova/Pages/PageAutorisation.xaml.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualBasic.ApplicationServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfAppGlazkovaTrufanova.Classes;
+
+namespace WpfAppGlazkovaTrufanova.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для PageAutorisation.xaml
+    /// </summary>
+    public partial class PageAutorisation : Page
+    {
+        public PageAutorisation()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSend_Click(object sender, RoutedEventArgs e)
+        {
+            string login = txtLogin.Text;
+            string password = psbPassword.Password;
+            using (GlazkovaTrufanovaContext tradeContext = new GlazkovaTrufanovaContext())
+            {
+                bool flag = false;
+                foreach (User user in tradeContext.Users.ToList())
+                    if (user.UserLogin == login)
+                        if (user.UserPassword == password)
+                        {
+                            MessageBox.Show("Добро пожаловать!");
+
+                            flag = true;
+                            ClassHelper.frmNavigate.Navigate(new PageProducts(user));
+                        }
+                if (!flag)
+                {
+                    MessageBox.Show("Не правильно введен логин или пароль");
+                }
+            }
+        }
+    }
+}
---
+++ WpfAppGlazkovaTrufanova/Pages/PageProducts.xaml.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualBasic.ApplicationServices;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfAppGlazkovaTrufanova.Classes;
+
+namespace WpfAppGlazkovaTrufanova.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для PageProducts.xaml
+    /// </summary>
+    public partial class PageProducts : Page
+    {
+        GlazkovaTrufanovaContext GTContext = new GlazkovaTrufanovaContext();
+        public PageProducts()
+        {
+            InitializeComponent();
+            txbFIO.Text = Dictionary.UserSurname + " " + Dictionary.UserName + " " + Dictionary.UserPatronymic;
+            lstViewProducts.ItemsSource = GTContext.Products.Include(t => t.Order).ToList();
+        }
+
+        private void txtSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string searchLine = txtSearch.Text;
+            if (!string.IsNullOrEmpty(searchLine))
+            {
+                lstViewProducts.ItemsSource = GTContext.Products.Include(t => t.Products).
+                Where(n => n.ProductName.Contains(searchLine) ||
+                n.ProductPrice.Contains(searchLine).ToList());
+            }
+            else
+            {
+                lstViewProducts.ItemsSource = GTContext.Products.Include(t => t.Products).ToList();
+            }
+        }
+
+        private void rbtDown_Checked(object sender, RoutedEventArgs e)
+        {
+            lstViewProducts.ItemsSource = GTContext.Products.Include(t => t.Products).OrderByDescending(x => x.ProductsPrice).ToList();
+        }
+
+        private void rbtUp_Checked(object sender, RoutedEventArgs e)
+        {
+            GlazkovaTrufanovaContext GTContext = new GlazkovaTrufanovaContext();
+            lstViewProducts.ItemsSource = GTContext.Products.Include(t => t.Products).OrderBy(x => x.ProductsPrice).ToList();
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void cmbName_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int numberProducts;
+
+            string nameProducts = cmbName.SelectedItem.ToString();
+
+            if (nameProducts == "Все производители")
+                numberProducts = 0;
+            else
+                numberProducts = GTContext.Manufacturers.FirstOrDefault
+                    (t => t.Name == nameProducts).ManufacturerId;
+            if (numberProducts == 0)
+            {
+                lstViewProducts.ItemsSource = GTContext.Products.Include
+                    (t => t.Products).ToList();
+            }
+            else
+            {
+                lstViewProducts.ItemsSource = GTContext.Products.Include
+                    (t => t.Products).Where(x => x.IDProducts == numberProducts).ToList();
+            }
+        }
+    }
+}
